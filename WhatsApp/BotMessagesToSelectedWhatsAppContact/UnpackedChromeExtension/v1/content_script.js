14dd65b08b057ac64ee7b9ebb39728bfa9147deb
Priyanshu Kumar Chaudhary <72826524+priyanshukdc@users.noreply.github.com>
2021-05-10 23:28:22 +0530

    Add files via upload

---
+++ WhatsApp/BotMessagesToSelectedWhatsAppContact/UnpackedChromeExtension/v1/content_script.js
@@ -0,0 +1,18 @@
+numOfMsgs = 15;
+try{
+for(i=1;i<=numOfMsgs;i++){
+	msgBox = document.querySelectorAll("[contenteditable='true']")[1];
+    	msg = Math.random().toString(36).substring(2, Math.max(8,15-i%3+48/i)); 
+    	msgBox.innerHTML=msg;
+    	event = new UIEvent("input",{    
+				    	bubbles : true,
+				    	cancelable : true,
+				    	view : window,
+			       	    	detail : 1   
+			             }); 
+        msgBox.dispatchEvent(event);
+        document.querySelectorAll("[data-icon='send']")[0].click();
+}"success";
+}catch(err){
+	"interrupted";
+}
