1450c7a80fb5406663956cf4e04f8206a984deca
Priyanshu Kumar Chaudhary <72826524+priyanshukdc@users.noreply.github.com>
2021-05-13 01:51:15 +0530

    Update instagramDpDownloadByVisitingSpecificProfile.js

--- Instagram/InstagramDpDownloadByVisitingSpecificProfile/instagramDpDownloadByVisitingSpecificProfile.js
+++ Instagram/InstagramDpDownloadByVisitingSpecificProfile/instagramDpDownloadByVisitingSpecificProfile.js
@@ -1,3 +1,12 @@
+/* Copyright (C) Priyanshu Kumar Chaudhary- All Rights Reserved
+ * You may use, distribute and modify this code
+ * if you keep this comments in file as it is and
+ * mention below lines in visible part of your content & description
+ * Title: “InstagramDpDownloadByVisitingSpecificProfile”
+ * Creator: “Priyanshu Kumar Chaudhary"
+ * Source: “Github :— https://github.com/priyanshukdc/cse_tricks/.
+ * Follow @thecodeattorney on Instagram for more such stuff. 
+ */
 fileName = window.location.href.split("/")[3]+".jpg";
 instaDp = document.querySelector("img[alt='Change Profile Photo']");
 if(instaDp == null)
