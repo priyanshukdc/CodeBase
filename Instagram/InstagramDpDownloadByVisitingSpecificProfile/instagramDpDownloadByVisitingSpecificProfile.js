f497fb1811f90ebfafff10356a49b427d177da72
Priyanshu Kumar Chaudhary <72826524+priyanshukdc@users.noreply.github.com>
2021-05-22 10:18:28 +0530

    Update instagramDpDownloadByVisitingSpecificProfile.js

--- Instagram/InstagramDpDownloadByVisitingSpecificProfile/instagramDpDownloadByVisitingSpecificProfile.js
+++ Instagram/InstagramDpDownloadByVisitingSpecificProfile/instagramDpDownloadByVisitingSpecificProfile.js
@@ -4,7 +4,7 @@
  * mention below lines in visible part of your content & description
  * Title: “InstagramDpDownloadByVisitingSpecificProfile”
  * Creator: “Priyanshu Kumar Chaudhary"
- * Source: “Github :— https://github.com/priyanshukdc/cse_tricks/.
+ * Source: “Github :— https://github.com/priyanshukdc/CodeBase/.
  * Follow @thecodeattorney on Instagram for more such stuff. 
  */
 fileName = window.location.href.split("/")[3]+".jpg";
